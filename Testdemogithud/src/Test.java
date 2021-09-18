bb85b7cba4059ab3d2978026c4e9f721b44292d2
klk123k123 <90376607+klk123k123@users.noreply.github.com>
2021-09-18 14:31:23 +0530

    Update Test.java

--- Testdemogithud/src/Test.java
+++ Testdemogithud/src/Test.java
@@ -3,7 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Lakshmi");
+		String name="Lakshmi"
+System.out.println("UpperCase  : "+name.toUpperCase());
 	}
 
 }
